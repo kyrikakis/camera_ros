a5346b55290803e3cc0d7930f626278309dee2d4
kiriks <kirikakis@gmail.com>
2023-11-29 09:14:07 +0000

    adding qos and change tppic name

--- src/CameraNode.cpp
+++ src/CameraNode.cpp
@@ -164,8 +164,31 @@ CameraNode::CameraNode(const rclcpp::NodeOptions &options) : Node("camera", opti
   // camera ID
   declare_parameter("camera", rclcpp::ParameterValue {}, param_descr_ro.set__dynamic_typing(true));
 
+  size_t depth = 10;
+  rmw_qos_reliability_policy_t reliability_policy = RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT;
+  rmw_qos_history_policy_t history_policy = RMW_QOS_POLICY_HISTORY_KEEP_LAST;
+
+  rmw_qos_profile_t qos_profile = rmw_qos_profile_default;
+
+  // Depth represents how many messages to store in history when the history policy is KEEP_LAST.
+  qos_profile.depth = depth;
+
+  // The reliability policy can be reliable, meaning that the underlying transport layer will try
+  // ensure that every message gets received in order, or best effort, meaning that the transport
+  // makes no guarantees about the order or reliability of delivery.
+  qos_profile.reliability = reliability_policy;
+
+  // The history policy determines how messages are saved until the message is taken by the reader.
+  // KEEP_ALL saves all messages until they are taken.
+  // KEEP_LAST enforces a limit on the number of messages that are saved, specified by the "depth"
+  // parameter.
+  qos_profile.history = history_policy;
+
+  auto qos = rclcpp::QoS(
+    rclcpp::QoSInitialization::from_rmw(qos_profile));
+
   // publisher for raw and compressed image
-  pub_image = this->create_publisher<sensor_msgs::msg::Image>("~/image_raw", 1);
+  pub_image = this->create_publisher<sensor_msgs::msg::Image>("/image", qos);
   pub_image_compressed =
     this->create_publisher<sensor_msgs::msg::CompressedImage>("~/image_raw/compressed", 1);
   pub_ci = this->create_publisher<sensor_msgs::msg::CameraInfo>("~/camera_info", 1);
